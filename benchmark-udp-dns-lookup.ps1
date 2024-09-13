fafedecb12266c8e0a3c7827109db0cdbc526c57
Rodney Beede <rbeede@users.noreply.github.com>
2024-09-12 20:48:29 -0500

    Add files via upload

---
+++ benchmark-udp-dns-lookup.ps1
@@ -0,0 +1,28 @@
+$nameresolvers = @(
+	# CloudFlare
+	"1.1.1.1",
+	"1.0.0.3",
+	"1.1.1.2",
+	# Google
+	"8.8.8.8",
+	"8.8.4.4",
+	# OpenDNS
+	"208.67.222.222",
+	"208.67.220.220",
+	# AdGuard
+	"94.140.14.14",
+	"94.140.15.15",
+	# CleanBrowsing
+	"185.228.168.9",
+	"185.228.169.9"	
+)
+
+while($true) {
+	$RandomNumber = Get-Random
+	
+	foreach ($resolver in $nameresolvers) {
+		nslookup ran$RandomNumber.rodney.dyndns.org  $resolver
+	}
+	
+	sleep 1
+}
